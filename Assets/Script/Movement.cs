b3fe82e9e708d58501bd34ecbc16a50ff8848cd4
BillyMRX1 <brilianadeputra@gmail.com>
2020-08-14 22:06:40 +0700

    Merging

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public string namaPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(namaPath),
+            "time", 5, 
+            "easetype", iTween.EaseType.easeInOutSine,
+            "looptype",iTween.LoopType.loop ));
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
