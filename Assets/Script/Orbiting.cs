95e22ef8b388ce0ece61429c91bd9b0ac3909e79
lazuardi100 <lazuardi.akhmad@gmail.com>
2020-08-15 20:02:12 +0700

    Bug Fixing + UI

--- Assets/Script/Orbiting.cs
+++ Assets/Script/Orbiting.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tes : MonoBehaviour
+public class Orbiting : MonoBehaviour
 {
+    public GameObject inti;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.RotateAround(inti.transform.position, Vector3.up, speed * Time.deltaTime);
     }
 }
